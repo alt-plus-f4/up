259f4571ef81d065b2c1a9c422ef281a1de62a66
alt-plus-f4 <valentin.assenov@abv.bg>
2025-10-03 09:46:56 +0300

    chore: fix sumbodules

---
+++ asd/intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int a = 0, b = 0, c = 0;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << c;
+}

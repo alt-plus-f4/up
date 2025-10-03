c008bbcd045b5794e37d66903e507c0bf54fabcb
alt-plus-f4 <valentin.assenov@abv.bg>
2025-10-03 10:35:05 +0300

    feat: finished lection

--- 03-10-uvod/intro.cpp
+++ 03-10-uvod/intro.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-
-int main(){
+int sum(){
     int a = 0, b = 0, c = 0;
 
     cout << "Enter a: ";
@@ -11,7 +11,37 @@ int main(){
     cout << "Enter b: ";
     cin >> b;
 
-    c = a + b;
+    return a + b;
+}
 
-    cout << c;
+int calc(){
+    int x = 0;
+
+    cout << "Enter x: ";
+    cin >> x;
+
+    return (sqrt(2 * (pow(x, 3))) + sin(2 * x)) / (abs((sin(x) + pow(cos(x), 2))) + (1 / 2));
+}
+
+int main(){
+    int result = sum();
+    cout << "Result: " << result << "\n";
+
+    int result2 = calc();
+    cout << "Result: " << result2 << "\n";       
+
+    if(result > result2) cout << "Result 1 is greater than Result 2\n";
+    else if(result < result2) cout << "Result 1 is less than Result 2\n";
+    else cout << "Result 1 is equal to Result 2\n";
+
+    cout << "Size of int: " << sizeof(int) << "\n";
+    cout << "Size of long int: " << sizeof(long int) << "\n";
+    cout << "Size of long long int: " << sizeof(long long int) << "\n";
+    cout << "Size of short int: " << sizeof(short int) << "\n";
+    cout << "Size of char: " << sizeof(char) << "\n";
+    cout << "Size of double: " << sizeof(double) << "\n";
+    cout << "Size of float: " << sizeof(float) << "\n";
+    cout << "Size of bool: " << sizeof(bool) << "\n";
+
+    return 0;
 }
